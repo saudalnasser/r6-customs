5b7309cd6db7a464949a5ca9502a131747d8b84e
SAUD ALNASSER <saud4services@gmail.com>
2022-06-30 08:33:57 +0300

    setup bot client initialization

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import 'dotenv/config';
 import config from './config';
+import Client, { ClientOptions } from './lib/client';
+import { LogLevel } from './lib/utils/logger/logger';
 import { createColors } from 'colorette';
 
 config.validate();
 
 createColors({ useColor: true });
+
+const options: ClientOptions = {
+  shards: 'auto',
+  intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES'],
+  logLevel: config.environment === 'development' ? LogLevel.Debug : LogLevel.Info,
+};
+
+const client: Client = new Client(options);
+
+client.run(config.token);
