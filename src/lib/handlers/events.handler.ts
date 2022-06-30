d0314b3184dc8feb0b645a1daefa81c6d0ac6955
SAUD ALNASSER <saud4services@gmail.com>
2022-06-30 07:10:42 +0300

    log events run errors

--- src/lib/handlers/events.handler.ts
+++ src/lib/handlers/events.handler.ts
@@ -1,4 +1,5 @@
 import { Client, ClientEvents, Collection } from 'discord.js';
+import { redBright, yellowBright } from 'colorette';
 import { importStructures } from '../utils/structure.utils';
 import Container from '../container';
 import Handler from './handler';
@@ -24,11 +25,41 @@ class EventsHandler implements Handler {
   private async handleEvents(client: Client): Promise<void> {
     for (const event of this.events.values()) {
       if (event.options.once) {
-        client.once(event.options.name, event.run);
+        client.once(event.options.name, async (...args): Promise<any> => {
+          try {
+            await event.run(...args);
+          } catch (error: any) {
+            this.container.logger.error(this.generateErrorMessage(client, event, error));
+          }
+        });
       } else {
-        client.once(event.options.name, event.run);
+        client.once(event.options.name, async (...args): Promise<any> => {
+          try {
+            await event.run(...args);
+          } catch (error: any) {
+            this.container.logger.error(this.generateErrorMessage(client, event, error));
+          }
+        });
       }
     }
+  }
+
+  private generateErrorMessage(
+    client: Client,
+    event: Event<keyof ClientEvents>,
+    error: Error
+  ): string {
+    const shard: string = `[${yellowBright(client.shard?.count ?? 0)}]`;
+    const eventName: string = `${yellowBright(event.options.name)}`;
+    const errorMessage: string = `${yellowBright(error?.message ?? '')}`;
+
+    const openBracket: string = redBright('<');
+    const closeBracket: string = redBright('>');
+
+    const styledEventName: string = `${openBracket}${eventName}${closeBracket}`;
+    const styledErrorMessage: string = `${redBright('error:')} ${errorMessage}`;
+
+    return `${shard} ${styledEventName} ${styledErrorMessage}`;
   }
 }
 
