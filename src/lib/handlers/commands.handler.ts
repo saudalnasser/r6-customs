3983c7121ea84b3d9f4e8fc294665675a764c079
SAUD ALNASSER <saud4services@gmail.com>
2022-06-30 07:46:25 +0300

    log commands run status

--- src/lib/handlers/commands.handler.ts
+++ src/lib/handlers/commands.handler.ts
@@ -1,4 +1,11 @@
-import { Client, Collection, CommandInteractionOptionResolver } from 'discord.js';
+import {
+  Client,
+  Collection,
+  CommandInteraction,
+  CommandInteractionOptionResolver,
+  GuildBasedChannel,
+} from 'discord.js';
+import { yellowBright, greenBright, redBright } from 'colorette';
 import { importStructures } from '../utils/structure.utils';
 import Container from '../container';
 import Handler from './handler';
@@ -24,13 +31,48 @@ class CommandsHandler implements Handler {
   private async handleCommands(client: Client): Promise<void> {
     client.on('interactionCreate', async (interaction) => {
       if (interaction.isCommand()) {
-        await this.commands.get(interaction.commandName)?.run({
-          client,
-          interaction,
-          args: interaction.options as CommandInteractionOptionResolver,
-        });
+        try {
+          await this.commands.get(interaction.commandName)?.run({
+            client,
+            interaction,
+            args: interaction.options as CommandInteractionOptionResolver,
+          });
+
+          this.container.logger.info(this.generateMessage('success', client, interaction));
+        } catch (error: any) {
+          this.container.logger.error(this.generateMessage('error', client, interaction, error));
+        }
       }
     });
+  }
+
+  private generateMessage(
+    type: 'success' | 'error',
+    client: Client,
+    interaction: CommandInteraction,
+    error?: Error
+  ): string {
+    const shard: string = `[${yellowBright(client.shard?.count ?? 0)}]`;
+    const member: string = `${yellowBright(interaction.member?.user.username ?? '')}`;
+    const command: string = `${yellowBright(interaction.commandName)}`;
+    const channel: string = `${yellowBright((interaction.channel as GuildBasedChannel).name)}`;
+    const errorMessage: string = `${yellowBright(error?.message ?? '')}`;
+
+    const at: string = type === 'success' ? greenBright('@') : redBright('@');
+    const openBracket: string = type === 'success' ? greenBright('<') : redBright('<');
+    const closeBracket: string = type === 'success' ? greenBright('>') : redBright('>');
+
+    const styledMember: string = `${at}${member}`;
+    const styledCommand: string = `${openBracket}${command}${closeBracket}`;
+    const styledChannel: string = `${openBracket}${channel}${closeBracket}`;
+    const styledErrorMessage: string = `${redBright('error:')} ${errorMessage}`;
+
+    switch (type) {
+      case 'success':
+        return `${shard} ${styledMember} ${styledCommand} ${styledChannel}`;
+      case 'error':
+        return `${shard} ${styledMember} ${styledCommand} ${styledChannel} ${styledErrorMessage}`;
+    }
   }
 }
 
