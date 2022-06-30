123bab9ac80594a462db2d6017abe1c374d29fad
SAUD ALNASSER <saud4services@gmail.com>
2022-06-30 07:56:26 +0300

    add structure import util method

---
+++ src/lib/utils/structure.utils.ts
@@ -0,0 +1,33 @@
+import { Collection } from 'discord.js';
+import { join } from 'path';
+import fs from 'fs';
+import Structure from '../structures/structure';
+
+type structureName = 'command' | 'event';
+
+async function importType(structureName: structureName, file: string): Promise<any> {
+  return (await import(`../../../dist/${structureName}s/${file}`))?.default;
+}
+
+/**
+ * imports all the structures of the given type and returns them in an array
+ * @param {structureName} structureName the name of the structure to import
+ * @returns array of structures of a specific type
+ */
+export async function importStructures<T extends Structure>(
+  structureName: structureName
+): Promise<Collection<string, T>> {
+  const collection: Collection<string, T> = new Collection();
+
+  const path: string = join(process.cwd(), 'dist', `${structureName}s`);
+  const files: string[] = fs.readdirSync(path);
+
+  for (const file of files) {
+    const Type: any = await importType(structureName, file);
+    const instance: T = new Type() as T;
+
+    collection.set(instance.options.name, instance);
+  }
+
+  return collection;
+}
