0bf8fe894a937caf1e779655228de8da6e854ab6
SAUD ALNASSER <saud4services@gmail.com>
2022-06-30 07:56:39 +0300

    create events handler

---
+++ src/lib/handlers/events.handler.ts
@@ -0,0 +1,29 @@
+import { Client, ClientEvents, Collection } from 'discord.js';
+import { importStructures } from '../utils/structure.utils';
+import Handler from './handler';
+import Event from '../structures/event.structure';
+
+class EventsHandler implements Handler {
+  public events!: Collection<string, Event<keyof ClientEvents>>;
+
+  public async initialize(client: Client): Promise<void> {
+    await this.loadEvents();
+    await this.handleEvents(client);
+  }
+
+  private async loadEvents(): Promise<void> {
+    this.events = await importStructures<Event<keyof ClientEvents>>('event');
+  }
+
+  private async handleEvents(client: Client): Promise<void> {
+    for (const event of this.events.values()) {
+      if (event.options.once) {
+        client.once(event.options.name, event.run);
+      } else {
+        client.once(event.options.name, event.run);
+      }
+    }
+  }
+}
+
+export default EventsHandler;
