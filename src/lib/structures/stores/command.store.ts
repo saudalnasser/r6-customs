df8ed36a804d98e53773d000db1461dcaa3b9622
SAUD ALNASSER <saud4services@gmail.com>
2022-07-04 07:06:36 +0300

    remove restiveness in the store subclasses

--- src/lib/structures/stores/command.store.ts
+++ src/lib/structures/stores/command.store.ts
@@ -1,31 +1,9 @@
-import { Collection } from 'discord.js';
 import Store from './store';
 import Command from '../pieces/command.piece';
 
 class CommandStore extends Store<Command> {
-  private commands!: Collection<string, Command>;
-
   public async initialize(): Promise<void> {
-    this.commands = await this.piecesLoader.load<Command>('command');
-  }
-
-  public get(name: string): Command | undefined {
-    return this.commands.get(name);
-  }
-
-  public getMany(names: string[]): Command[] {
-    const commands: Command[] = [];
-
-    for (const name of names) {
-      const command: Command | undefined = this.commands.get(name);
-      if (command) commands.push(command);
-    }
-
-    return commands;
-  }
-
-  public getIterable(): IterableIterator<Command> {
-    return this.commands.values();
+    this.pieces = await this.piecesLoader.load<Command>('command');
   }
 }
 
--- src/lib/structures/stores/store.ts
+++ src/lib/structures/stores/store.ts
@@ -1,20 +1,38 @@
-import Container from '../container';
+import { Collection } from 'discord.js';
 import Structure from '../structure';
 import Piece from '../pieces/piece';
+import Container from '../container';
 import PiecesLoader from '../../utils/pieces/pieces-loader';
 
 abstract class Store<T extends Piece> extends Structure {
+  protected pieces!: Collection<string, T>;
   protected piecesLoader: PiecesLoader;
-
-  public abstract initialize(): Promise<void>;
-  public abstract get(name: string): T | undefined;
-  public abstract getMany(names: string[]): T[];
-  public abstract getIterable(): IterableIterator<T>;
 
   public constructor(container: Container, piecesLoader: PiecesLoader) {
     super(container);
 
     this.piecesLoader = piecesLoader;
+  }
+
+  public abstract initialize(): Promise<void>;
+
+  public get(name: string): T | undefined {
+    return this.pieces.get(name);
+  }
+
+  public getMany(names: string[]): T[] {
+    const pieces: T[] = [];
+
+    for (const name of names) {
+      const piece: T | undefined = this.pieces.get(name);
+      if (piece) pieces.push(piece);
+    }
+
+    return pieces;
+  }
+
+  public getIterable(): IterableIterator<T> {
+    return this.pieces.values();
   }
 }
 
