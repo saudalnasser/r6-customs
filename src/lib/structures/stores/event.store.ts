9371c5079cf9e9c2e31b396565ebe7a1e132ed03
SAUD ALNASSER <saud4services@gmail.com>
2022-07-04 07:06:36 +0300

    remove restiveness in the store subclasses

--- src/lib/structures/stores/event.store.ts
+++ src/lib/structures/stores/event.store.ts
@@ -1,31 +1,10 @@
-import { ClientEvents, Collection } from 'discord.js';
+import { ClientEvents } from 'discord.js';
 import Store from './store';
 import Event from '../pieces/event.piece';
 
 class EventStore extends Store<Event<keyof ClientEvents>> {
-  private events!: Collection<string, Event<keyof ClientEvents>>;
-
   public async initialize(): Promise<void> {
-    this.events = await this.piecesLoader.load<Event<keyof ClientEvents>>('event');
-  }
-
-  public get(name: string): Event<keyof ClientEvents> | undefined {
-    return this.events.get(name);
-  }
-
-  public getMany(names: string[]): Event<keyof ClientEvents>[] {
-    const events: Event<keyof ClientEvents>[] = [];
-
-    for (const name of names) {
-      const event: Event<keyof ClientEvents> | undefined = this.events.get(name);
-      if (event) events.push(event);
-    }
-
-    return events;
-  }
-
-  public getIterable(): IterableIterator<Event<keyof ClientEvents>> {
-    return this.events.values();
+    this.pieces = await this.piecesLoader.load<Event<keyof ClientEvents>>('event');
   }
 }
 
--- src/lib/structures/stores/guard.store.ts
+++ src/lib/structures/stores/guard.store.ts
@@ -1,31 +1,9 @@
-import { Collection } from 'discord.js';
 import Store from './store';
 import Guard from '../pieces/guard.piece';
 
 class GuardStore extends Store<Guard> {
-  private guards!: Collection<string, Guard>;
-
   public async initialize(): Promise<void> {
-    this.guards = await this.piecesLoader.load<Guard>('guard');
-  }
-
-  public get(name: string): Guard | undefined {
-    return this.guards.get(name);
-  }
-
-  public getMany(names: string[]): Guard[] {
-    const guards: Guard[] = [];
-
-    for (const name of names) {
-      const event: Guard | undefined = this.guards.get(name);
-      if (event) guards.push(event);
-    }
-
-    return guards;
-  }
-
-  public getIterable(): IterableIterator<Guard> {
-    return this.guards.values();
+    this.pieces = await this.piecesLoader.load<Guard>('guard');
   }
 }
 
