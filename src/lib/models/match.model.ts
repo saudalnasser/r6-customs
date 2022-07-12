242efa7afb4ed0c8b97d11b4b89860ac251dc537
SAUD ALNASSER <saud4services@gmail.com>
2022-07-12 21:58:00 +0300

    create match model

---
+++ src/lib/models/match.model.ts
@@ -0,0 +1,35 @@
+import { IPlayer } from './player.model';
+import { Model, model, Schema, Types } from 'mongoose';
+import Maps from '../types/maps.enum';
+
+export type MatchState = 'ongoing' | 'resolved';
+
+export type Winner = 'teamA' | 'teamB';
+
+export interface IMatch {
+  _id?: Types.ObjectId;
+  teamA: Types.Array<IPlayer>;
+  teamB: Types.Array<IPlayer>;
+  host: IPlayer;
+  map: Maps;
+  state: MatchState;
+  winner?: Winner;
+  timestamps: { createdAt: Date; resolvedAt?: Date };
+}
+
+const schema: Schema<IMatch> = new Schema<IMatch>({
+  teamA: [{ type: Schema.Types.ObjectId, ref: 'Player', required: true }],
+  teamB: [{ type: Schema.Types.ObjectId, ref: 'Player', required: true }],
+  host: { type: Schema.Types.ObjectId, ref: 'Player', required: true },
+  map: { type: String, enum: Object.values(Maps), require: true },
+  state: { type: String, require: true },
+  winner: { type: String },
+  timestamps: {
+    createdAt: { type: Date, require: true },
+    resolvedAt: { type: Date },
+  },
+});
+
+const Match: Model<IMatch> = model<IMatch>('Match', schema);
+
+export default Match;
---
+++ src/lib/types/maps.enum.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+
+enum Maps {
+  Border = 'border',
+  Chalet = 'chalet',
+  Clubhouse = 'clubhouse',
+  Coastline = 'coastline',
+  Bank = 'bank',
+  Kafe = 'kafe',
+  Oregon = 'oregon',
+  Skyscraper = 'skyscraper',
+  ThemePark = 'themePark',
+  Villa = 'villa',
+}
+
+export default Maps;
